f00b7ee9bc358a2282356d76dd96e34ef623bdcb
Markel Sainz <markelsainz@gmail.com>
2021-12-22 19:20:31 +0100

    Initial commit. Created models for CRM

---
+++ amcrm/crmapi/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+import os
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=32, blank=False)
+    password = models.CharField(max_length=32, blank=False)
+    id = models.AutoField(primary_key=True)
+    isAdmin = models.BooleanField(default=False, blank=False)
+
+def get_image_path(instance, filename):
+    return os.path.join('customers', str(instance.id), filename)
+
+class Customer(models.Model):
+    name = models.CharField(max_length=60, blank=False)
+    surname = models.CharField(max_length=60, blank=False)
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
