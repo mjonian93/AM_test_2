2be6def48c22039827f18ab199d53eeac2b45c3e
Markel Sainz <markelsainz@gmail.com>
2021-12-25 19:37:22 +0100

    Updated the URLs for indexed CRUD

--- amcrm/crmapi/urls.py
+++ amcrm/crmapi/urls.py
@@ -4,6 +4,8 @@ from .views import CustomerAPIView, UserAPIView
 
 urlpatterns = [
     path('customers/', CustomerAPIView.as_view(), name='api_customer'),
+    path('customers/<int:id>', CustomerAPIView.as_view(), name='api_customer_indexed'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    path('users/', UserAPIView.as_view(), name='api_user')
+    path('users/', UserAPIView.as_view(), name='api_user'),
+    path('users/<int:id>', UserAPIView.as_view(), name='api_user_indexed')
 ]
